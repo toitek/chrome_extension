6724ff15e64585f308c5dc8d2a89c804957a79db
kabingusam <kabingusammy@gmail.com>
2023-01-24 13:42:46 +0300

    pop up apperance completion

--- scripts/eventPage.js
+++ scripts/eventPage.js
@@ -4,14 +4,17 @@ var menuItem = {
     "contexts": ["selection"]
 };
 
-// context menu 
 chrome.contextMenus.create(menuItem);
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
     if (info.menuItemId === "AIwriter") {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { text: info.selectionText }, function (response) {
-            });
+        chrome.windows.create({
+            url: "ui/popup.html",
+            type: "popup",
+            width: 600,
+            height: 400
+        }, function (window) {
+            chrome.runtime.sendMessage({ selectedText: info.selectionText });
         });
     }
 });

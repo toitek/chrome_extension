8d877357dc3a2dbbb81ed497c0d2deb4788ef726
Sylvester254 <slynganga59@gmail.com>
2023-01-25 18:01:55 +0300

    Autocomplete working

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,4 @@
+
 let textArea = document.querySelector(".Am.Al.editable.LW-avf");
 if (!textArea) {
     textArea = document.querySelector(".kix-appview-editor.kix-lineview-editor");
@@ -20,15 +21,19 @@ document.addEventListener("focusin", async (event) => {
 });
 
 async function enableExtension(textArea) {
+  let suggestionAccepted = false;
   textArea.addEventListener("keydown", async (event) => {
       // Check if the pressed key is the space bar
-      if (event.code === "Space") {
+      if (event.code === "Space" && event.code !== ["Space", "Space"]) {
           // Get the text before the cursor
-          const text = textArea.value.substring(0, textArea.selectionStart);
-          // Get the last word before the cursor
-          const lastWord = text.split(" ").pop();
-          // Autocomplete the word using GPT-3 API
-          try {
+        const text = textArea.value.substring(0, textArea.selectionStart);
+        // Split the text into an array of words
+        const words = text.split(" ");
+        // Get the last four words
+        const lastFourWords = words.slice(-4);
+        // Use the last four words to understand the context of the text
+        const context = lastFourWords.join(" ");
+        // Autocomplete the word using GPT-3 API
               const response = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
                 method: 'POST',
                 headers: {
@@ -36,9 +41,9 @@ async function enableExtension(textArea) {
                   'Authorization': 'Bearer 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'
                 },
                 body: JSON.stringify({
-                  prompt: lastWord,
-                  max_tokens: 7,
-                  temperature: 0.7
+                  prompt: context,
+                  max_tokens: 1,
+                  temperature: 0.89
                 })
               });
               const data = await response.json();
@@ -46,31 +51,31 @@ async function enableExtension(textArea) {
                 autocompletedText = data.choices[0].text;
                 // Insert the autocompleted text into the text area
                 textArea.value = text + autocompletedText;
-                // highlight the autocompleted text
+                // Change the color of the autocompleted text to grey
                 autocompletedTextStart = text.length;
                 autocompletedTextEnd = textArea.value.length;
                 textArea.setSelectionRange(autocompletedTextStart, autocompletedTextEnd);
-document.execCommand("hiliteColor", false, "yellow");
-
-                textArea.classList.add("highlight");
-                // Move the cursor to the end of the autocompleted text
-                textArea.selectionStart = textArea.value.length;
-                textArea.selectionEnd = textArea.value.length;
+                document.execCommand("ForeColor", false, "grey");
             } else {
                 console.log("Data returned from the API is null or undefined")
             }
-          } catch (error) {
-              console.error(error);
-          }
       }
-  });
-  textArea.addEventListener("keydown", (event) => {
-    // Check if the pressed key is the Enter key
-    if (event.code === "Enter") {
-        // include the autocompleted text
-        autocompletedText = "";
-        //remove the highlight
-        textArea.classList.remove("highlight");
+      if (event.code === "Enter") {
+        suggestionAccepted = true;
     }
+  });
+  textArea.addEventListener("keyup", (event) => {
+    if (suggestionAccepted) {
+      // Change the color of the autocompleted text to default
+      textArea.setSelectionRange(autocompletedTextStart, autocompletedTextEnd);
+      document.execCommand("ForeColor", false, "default");
+      // Move the cursor to the end of the autocompleted text
+      textArea.selectionStart = textArea.value.length;
+      textArea.selectionEnd = textArea.value.length;
+  } else {
+      // Discard the autocomplete suggestion
+      autocompletedText = "";
+  }
+  suggestionAccepted = false;
 });
 }

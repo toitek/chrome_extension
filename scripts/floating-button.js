03e1b17e9b6b7a61df0d3755c56d59a429092428
kabingusam <kabingusammy@gmail.com>
2023-01-26 18:20:01 +0300

    tooltip appearance

--- scripts/floating-button.js
+++ scripts/floating-button.js
@@ -9,25 +9,58 @@ options.style.display = "none";
 
 // Create options
 const option1 = document.createElement("div");
-option1.innerHTML = "Disable";
+option1.innerHTML = "Enable";
 options.appendChild(option1);
 
 const option2 = document.createElement("div");
-option2.innerHTML = "Stop";
+option2.innerHTML = "Disable";
 options.appendChild(option2);
 
-const option3 = document.createElement("div");
-option3.innerHTML = "Login";
-options.appendChild(option3);
 
 // Append options to button
 button.appendChild(options);
 
 // Show options when button is hovered
 button.addEventListener("mouseover", () => {
-    options.style.display = "block";
+    options.style.display = "flex";
+    options.style.backgroundColor = "white";
+    options.style.border = "1px solid #4CAF50";
+    options.style.padding = "10px";
+    options.style.borderRadius = "5px";
     options.style.transition = "all 0.5s ease-in-out";
-    options.style.left = "-150px";
+    options.style.left = "-100px";
+    options.style.position = "absolute";
+    options.style.zIndex = "1";
+    option1.style.marginRight = "5px";
+    option2.style.marginRight = "5px";
+    option3.style.marginRight = "5px";
+    option1.style.backgroundColor = "#4CAF50";
+    option2.style.backgroundColor = "#4CAF50";
+    option3.style.backgroundColor = "#4CAF50";
+    option1.style.color = "white";
+    option2.style.color = "white";
+    option3.style.color = "white";
+    option1.style.padding = "5px";
+    option2.style.padding = "5px";
+    option3.style.padding = "5px";
+    option1.style.borderRadius = "5px";
+    option2.style.borderRadius = "5px";
+    option3.style.borderRadius = "5px";
+    option1.style.cursor = "pointer";
+    option2.style.cursor = "pointer";
+    options.style.backgroundColor = "#f2f2f2";
+    options.style.borderRadius = "4px";
+    options.style.padding = "8px";
+    options.style.position = "absolute";
+    options.style.zIndex = "1";
+    options.style.boxShadow = "0 4px 8px 0 rgba(0,0,0,0.2)";
+    options.style.top = "30px";
+    option1.style.cursor = "pointer";
+    option1.style.margin = "4px 0";
+    option2.style.cursor = "pointer";
+    option2.style.margin = "4px 0";
+    option3.style.cursor = "pointer";
+    option3.style.margin = "4px 0";
 });
 
 // Hide options when button is not hovered
@@ -44,6 +77,9 @@ button.addEventListener("click", () => {
         active: true
     });
 });
+
+// make the button draggable
+
 
 // check for text areas to appear
 function checkForFocus() {
